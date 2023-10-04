02952e32ba2546dca2cc10263514282ad86a64b4
Khainsaa <146912378+Khainsaa@users.noreply.github.com>
2023-10-04 12:01:01 +0700

    Create hello.c
    Program pertama yang saya buat

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+	int m,n,r;
+	
+  printf("Masukan nilai M : ");
+	scanf("%d",&m);
+	printf("Masukan nilai N : ");
+	scanf("%d",&n);
+
+	while (n != 0) {
+		r = m%n;
+		m = n;
+		n = r;
+	}
+	printf("GCD : %d\n",m);
+}
