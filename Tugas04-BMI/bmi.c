7ca7ad72de59e3b674e5c3134adda3e901249f2b
Khainsaa <khairunnisaanaqah@gmail.com>
2023-10-25 10:31:51 +0700

    baru

--- Tugas04-BMI/bmi.c
+++ Tugas04-BMI/bmi.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 int main() {
-    char jenisKelamin;
+    int pilihan, jenisKelamin;
     float tinggi, berat, bmi;
-
-    printf("===INPUT===\n");
-    printf("Pilih Jenis Kelamin (P/Pria, W/Wanita): ");
-    scanf(" %c", &jenisKelamin);
-
-    switch (jenisKelamin) {
-        case 'P':
-        case 'p':
-            printf("Masukkan Tinggi Badan (Meter): ");
+    //Input
+	printf("===KALKULATOR BMI===\n");
+	printf("1. Wanita\n2. Pria\n");
+	printf("Pilihan: "); scanf("%d", &pilihan);
+    //Proses percabangan
+	switch (pilihan) {
+		case 1:
+        printf("Masukkan Tinggi Badan (Meter): ");
             scanf("%f", &tinggi);
 
             printf("Masukkan Berat Badan (Kilogram): ");
@@ -27,15 +25,13 @@ int main() {
                 printf("Badan Tidak Ideal\n");
             }
             break;
-
-        case 'W':
-        case 'w':
-            printf("Masukkan Tinggi Badan (Meter): ");
+        case 2:
+        printf("Masukkan Tinggi Badan (Meter): ");
             scanf("%f", &tinggi);
 
             printf("Masukkan Berat Badan (Kilogram): ");
             scanf("%f", &berat);
-            
+
             bmi = berat / (tinggi * tinggi);
             printf("===OUTPUT===\n");
             printf("BMI Anda: %.2f\n", bmi);
@@ -46,7 +42,8 @@ int main() {
                 printf("Badan Tidak Ideal\n");
             }
             break;
+            default :
+            printf("Pilihan jenis kelamin tidak valid. \n");
+            break;
     }
-
-return 0;
 }
